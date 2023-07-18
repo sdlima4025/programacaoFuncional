e1023eafdca9eb4ee68f1fdfc3e26fd4e979add0
sdlima4025 <sergiolimafg3@gmail.com>
2023-07-18 11:41:56 -0300

    herança Funcional

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let person1 = createPerson('Sérgio', 'Lima', 52);
 let person2 = createPerson('Silvio', 'Santana', 56);
 
 console.log(person1, person2);
-*/
+
 
 // Funcional: Instância e This
 let person = {
@@ -24,6 +24,35 @@ let person = {
   age: 52,
   getFullName() {
       return `Seu nome é: ${this.name} ${this.lastName} e você tem ${this.age} anos`;
+  },
+  start() {
+    console.log('deu start na pessoa!');
   }
 }
+person.start();
 console.log(person.getFullName());
+*/
+
+// Funcional: Herança
+// Objeto Padrão
+const defaultUser = {
+  name:'',
+  email:'',
+  level: 1
+}
+
+// operador spread ==...nome da variavél clona o objeto
+let user1 = {
+  ...defaultUser,
+  name:'Sérgio',
+  email:'sergiolima@gmail.com'
+}
+console.log(user1);
+
+let adm1 = {
+  ...defaultUser,
+  name: 'admin',
+  email: 'admin@gmail.com.br',
+  level: 2
+}
+console.log(adm1);
